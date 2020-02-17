abac6c5b6ed4d443fabf9acae0b8dadaf6d9ef96
Vitor Vicente <vitorvicentecorreia@gmail.com>
2020-02-16 21:29:32 -0300

    testando o navigator.permissions

--- src/pages/WeatherPage/index.js
+++ src/pages/WeatherPage/index.js
@@ -24,19 +24,19 @@ export default function WeatherPage() {
         weatherData.currently = {};
 
         navigator.permissions.query({ name: "geolocation" }).then(result => {
-            alert(result.state);
             navigator.geolocation.getCurrentPosition();
             if (result.state === "granted" && result.state === "prompt") {
-                // navigator.geolocation.getCurrentPosition(async position => {
-                //     const { latitude, longitude } = position.coords;
-                //     const weatherRequest = await darkSkyApi.get(
-                //         `${latitude},${longitude}`
-                //     );
-                //     const weatherInfoRequest = weatherInfoCreator(
-                //         weatherRequest.data
-                //     );
-                //     setWeatherInfo(weatherInfoRequest);
-                // });
+                navigator.geolocation.getCurrentPosition(position => {
+                    const { latitude, longitude } = position.coords;
+                    alert(latitude, longitude);
+                    // const weatherRequest = await darkSkyApi.get(
+                    //     `${latitude},${longitude}`
+                    // );
+                    // const weatherInfoRequest = weatherInfoCreator(
+                    //     weatherRequest.data
+                    // );
+                    // setWeatherInfo(weatherInfoRequest);
+                });
             }
         });
     }, []);
